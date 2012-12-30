7c01748b48340170b197dca6ab5834c014ea9332
Jakob Flierl <jakob.flierl@gmail.com>
2012-12-30 18:14:44 +0100

    add I2C PCF8574 connection.

---
+++ Tests/AccelStepper/Bounce/sim.cpp
@@ -0,0 +1,67 @@
+#include <fcntl.h>  // For O_RDWR
+#include <unistd.h> // For open(), close()
+#include <assert.h> // For assert()
+
+#include <I2CIO.h>  // I2C port expander access
+
+/**
+ * sim.cpp - Simulator specific functions.
+ *
+ * setupSim(argc, argv) - is called during the startup of the simulator.
+ * pinMode(pin, mode)   - maps the Arduino pin 13 to I2C port expander pin 0.
+ * digitalWrite(p, val) - maps the Arduino pin 13 to I2C port expander pin 0. 
+ *
+ * usage: 01-Blink.exe [/dev/i2c-?] [I2C address]
+ *
+ * Example:
+ *
+ * 01-Blink.exe /dev/i2c-9 0x38
+ *
+ */
+
+I2CIO *io;
+
+int setupSim(int argc, char** argv)
+{
+  int fd;
+
+  if (!(argc > 2)) {
+	fprintf(stderr,
+			"usage: %s [/dev/i2c-?] [I2C address]\n",
+			basename(argv[0]));
+	return 0;
+  }
+
+  fd = open(argv[1], O_RDWR);
+
+  if (!(fd >= 0)) {
+	fprintf(stderr, "Error while opening %s. Exiting\n", argv[1]);
+	return 2;
+  }
+
+  int i2c_addr = strtol(argv[2], NULL, 16);
+
+  io = new I2CIO(fd);
+
+  io->begin(i2c_addr);
+  io->portMode(OUTPUT);
+  io->write(0xff);
+
+  return 0;
+}
+
+void pinMode(uint8_t p, uint8_t mode)
+{
+  if (p >= 2 && p <= 9) {
+	if (io) io->pinMode(p-2, mode);
+  }
+}
+
+void digitalWrite(uint8_t p, uint8_t v)
+{
+  debugf("digitalWrite(%d, %d)\n", p, v);
+
+  if (p >= 2 && p <= 9) {
+	if (io) io->digitalWrite(p-2, v);
+  }
+}
